9e8fd6c2978e24a022f95695580d2d20eb33ed06
earth28 <jiu9659@gmail.com>
2026-03-21 23:09:44 +0900

    Add comments

--- 2026.03/3월 3주차/전화번호_목록.java
+++ 2026.03/3월 3주차/전화번호_목록.java
@@ -3,9 +3,12 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        
+
+        // 배열 정렬
         Arrays.sort(phone_book);
-        
+
+        // 앞 번호가 다음 번호의 prefix인지 확인
+        // startsWith() : 대상 문자열이 특정 문자 또는 문자열로 시작하는지 체크하는 함수
         for (int i = 1; i < phone_book.length; i++) {
             if (phone_book[i].startsWith(phone_book[i - 1])) {
                 answer = false;
